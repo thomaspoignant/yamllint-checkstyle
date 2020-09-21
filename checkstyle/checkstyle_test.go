8f1a1914e403da26bb1eb308b17679979071f5d6
Thomas Poignant <thomas.poignant@adevinta.com>
2020-09-21 14:12:48 +0200

    Linter fix

--- checkstyle/checkstyle_test.go
+++ checkstyle/checkstyle_test.go
@@ -24,7 +24,10 @@ func TestBuildCheckStyle(t *testing.T) {
 	})
 
 	// Check the output
-	want:= `<checkstyle version="1.0.0"><file name="path/to/file"><error line="10" column="5" severity="error" message="msg1" source="test"></error><error line="11" column="58" severity="debug" message="msg2" source="test"></error></file></checkstyle>`
+	want:= "<checkstyle version=\"1.0.0\"><file name=\"path/to/file\"><error line=\"10\" column=\"5\" severity=\"error\"" +
+		" message=\"msg1\" source=\"test\"></error><error line=\"11\" column=\"58\" severity=\"debug\" message=\"msg2\" " +
+		"source=\"test\"></error></file></checkstyle>"
+
 	if checkStyle.String() != want {
 		t.Errorf("Wrong output for String()\n\twant: %s\n\tgot: %s",want,checkStyle.String())
 	}
